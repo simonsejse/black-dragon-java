618c35171b867dc75e5d88c66f5aaa29a61eeaf7
SimonWin <simonwa01@gmail.com>
2023-03-24 12:30:52 +0100

    Finished black dragon

---
+++ src/main/java/dk/acto/blackdragon/ModelTransformerImpl.java
@@ -0,0 +1,12 @@
+package dk.acto.blackdragon.service;
+
+import dk.acto.blackdragon.model.Model;
+import dk.acto.blackdragon.model.Stats;
+import io.vavr.collection.List;
+
+public class ModelTransformerImpl2 implements ModelTransformer<Model, Stats> {
+    @Override
+    public Stats transform(List<Model> model) {
+        return null;
+    }
+}
